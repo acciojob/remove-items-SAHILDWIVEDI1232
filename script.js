fef38f768fdcd09bf663c2598fb348262fe69b3b
SAHILDWIVEDI1232 <168732444+SAHILDWIVEDI1232@users.noreply.github.com>
2025-08-29 11:40:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+document.addEventListener("DomLoad",function(){
+let select=document.getElementById("colorSelect");
+			
+let button=document.querySelector(['input type="button"]'];
+	button.addEventListener("click",function(){
+		let si=select.si;
+		if(si!==-1){
+			select.remove(si);
+		}
+	});
+});
